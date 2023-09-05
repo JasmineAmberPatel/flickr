3c6fbde84fda245c29bf7e8007bdf5d4b08db568
Jasmine Patel <jasmine.patel@footasylum.com>
2023-09-05 21:52:39 +0100

    Initial Commit

---
+++ flickr/flickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  flickrApp.swift
+//  flickr
+//
+//  Created by Jasmine Patel on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct flickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
