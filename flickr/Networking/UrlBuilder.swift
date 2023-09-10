ecd48e1c0032a6d4a5be54883403d2f2ab22b7c6
Jasmine Patel <jasmine.patel@footasylum.com>
2023-09-10 18:48:08 +0100

    refactor urlBuilder

--- flickr/Networking/UrlBuilder.swift
+++ flickr/Networking/UrlBuilder.swift
@@ -12,10 +12,8 @@ struct UrlBuilder {
     private var host: String = "https://www.flickr.com"
     private var path: String = "/services/rest/"
     private var finalQueryString: String = "&safe_search=1&format=json&nojsoncallback=1"
-    private var url: String
     
     mutating func urlBuilder(method: String, params: String) -> String {
-        url = ("\(host)\(path)?method=\(method)&api_key=\(apiKey)\(params)\(finalQueryString)")
-        return url
+        return ("\(host)\(path)?method=\(method)&api_key=\(apiKey)\(params)\(finalQueryString)")
     }
 }
