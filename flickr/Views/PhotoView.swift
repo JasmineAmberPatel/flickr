130950ae0fef362e4f4397a61562722a1bd4951e
Jasmine Patel <jasmine.patel@footasylum.com>
2023-09-11 12:58:28 +0100

    Edit styling of user icon and name

--- flickr/Views/PhotoView.swift
+++ flickr/Views/PhotoView.swift
@@ -14,6 +14,21 @@ struct PhotoView: View {
     
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
+            HStack {
+                AsyncImage(url: URL(string: viewModel.userDetails.person?.iconUrl ?? "")) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 30, height: 30)
+                Text(viewModel.userDetails.person?.username.content ?? "")
+                    .font(.caption)
+                Spacer()
+            }
+            .padding(.leading, 10)
             AsyncImage(url: URL(string: photo.photoUrl)) { image in
                 image
                     .resizable()
@@ -22,24 +37,13 @@ struct PhotoView: View {
                 ProgressView()
                     .frame(width: 100, height: 100)
             }
-            .padding()
-            HStack {
-                AsyncImage(url: URL(string: viewModel.userDetails.person?.iconUrl ?? "")) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                } placeholder: {
-                    ProgressView()
-                }
-                .frame(width: 30, height: 30)
-                Text(viewModel.userDetails.person?.username.content ?? "")
-            }
+            .padding(10)
         }
     }
 }
 
-//struct PhotoView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PhotoView(PhotosViewModel())
-//    }
-//}
+struct PhotoView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoView(viewModel: PhotosViewModel(), photo: PhotoElement())
+    }
+}
