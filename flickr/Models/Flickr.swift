88fbfdfa98153a8a645e2d374eae7090400ec3cd
Jasmine Patel <jasmine.patel@footasylum.com>
2023-09-11 11:17:48 +0100

    Put photo search structs together to make project navigation easier

--- flickr/Models/Flickr.swift
+++ flickr/Models/Flickr.swift
@@ -7,7 +7,27 @@
 
 import Foundation
 
+// MARK: Flickr
 struct Flickr: Codable {
-    var photos: Photos?
+    var photos: PhotosList?
     var stat: String = ""
+}
+
+// MARK: PhotosList
+struct PhotosList: Codable {
+    var photo: [PhotoElement]?
+}
+
+// MARK: PhotoElement
+struct PhotoElement: Codable, Hashable {
+    var id: String = ""
+    var owner: String = ""
+    var secret: String = ""
+    var server: String = ""
+    var farm: Int = 0
+    var title: String = ""
+    
+    var photoUrl: String {
+        return "https://farm\(farm).staticflickr.com/\(server)/\(id)_\(secret).jpg"
+    }    
 }
