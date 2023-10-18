694ee4f2afc4b269c907dccf956838fb9c15ee43
Jasmine <jasmineamber.ja@gmail.com>
2023-10-18 17:12:39 +0100

    Add UI to Image Grid view

--- flickr/Views/ImageGridView.swift
+++ flickr/Views/ImageGridView.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 
 struct ImageGridView: View {
+    var authorPhotos: AuthorPhotos
+    
     var body: some View {
-        VStack {
-            Text("This will be a stack of images")
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 0))], spacing: 10) {
+                if let photos = authorPhotos.authorPhotosList?.photo {
+                    ForEach(photos, id: \.self) { photo in
+                        PhotoView(photo: photo)
+                    }
+                }
+            }
+            .padding(.horizontal)
         }
     }
 }
 
 struct ImageGridView_Previews: PreviewProvider {
     static var previews: some View {
-        ImageGridView()
+        ImageGridView(authorPhotos: AuthorPhotos())
     }
 }
