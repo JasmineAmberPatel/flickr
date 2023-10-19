6639d3743d775500bf05ae47c2fa0eae2c5b09f4
Jasmine <jasmineamber.ja@gmail.com>
2023-10-19 15:54:24 +0100

    Moved PhotoList struct into it's own file so it can be more easily reused

---
+++ flickr/Models/PhotoList.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoList.swift
+//  flickr
+//
+//  Created by Jasmine Patel on 19/10/2023.
+//
+
+import Foundation
+
+struct PhotosList: Codable, Equatable {
+    var photo: [PhotoElement]?
+}
